8a9ab7939cbafe702c7d44335f2608fd1e3d0550
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-12 18:35:47 -0300

    feat: Add Task and User models

---
+++ src/main/java/com/rocketseat/todolist/model/Task.java
@@ -0,0 +1,38 @@
+package com.rocketseat.todolist.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class Task {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(length = 50)
+    private String title;
+
+    private String description;
+
+    private LocalDateTime startAt;
+
+    private LocalDateTime endAt;
+
+    private String priority;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID idUser;
+
+}
